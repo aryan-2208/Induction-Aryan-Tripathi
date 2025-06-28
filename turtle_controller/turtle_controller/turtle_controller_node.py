70e822a9cbca53f886ba68ca75ffffca078e8715
Aryan Tripathi <tripathiaryan228@gmail.com>
2025-06-28 17:48:27 +0530

    Manually populate turtle_controller folder

---
+++ turtle_controller/turtle_controller/turtle_controller_node.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class TurtleController(Node):
+    def __init__(self):
+        super().__init__('turtle_controller')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.move_turtle)
+        self.get_logger().info("TurtleController started")
+
+    def move_turtle(self):
+        msg = Twist()
+        msg.linear.x = 0.1
+        msg.angular.z = 0.25
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: linear.x={msg.linear.x}, angular.z={msg.angular.z}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtleController()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
